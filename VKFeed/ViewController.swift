2c2f5b39e4bcb0697f83c2656e32f48b941c4738
lilcappucc <lilcappucc@gmail.com>
2020-01-13 20:21:32 +0200

    Initial Commit

---
+++ VKFeed/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VKFeed
+//
+//  Created by dewill on 13.01.2020.
+//  Copyright © 2020 lilcappucc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
